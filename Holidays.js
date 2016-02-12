67650a8ee199247f2fa9453d6ba24d2fdd7cd702
tristan-lin <tristan-lin@users.noreply.github.com>
2016-02-12 17:34:05 +0900

    Initial relase version of jQueryGantt for Kintone plugin

---
+++ Holidays.js
@@ -0,0 +1,36 @@
+/*
+Seperate the isHoliday() an independent file for 
+easy configuring. 
+*/
+
+// year start 
+ var holidays = "#01_01"
+     holidays = holidays + "#01_02"
+     holidays = holidays + "#01_03"
+
+//   for 2016 
+     holidays = holidays + "#01_11"
+     holidays = holidays + "#02_11"
+     holidays = holidays + "#03_21"
+     holidays = holidays + "#04_29"
+     holidays = holidays + "#04_29"
+     holidays = holidays + "#05_03"
+     holidays = holidays + "#05_04"
+     holidays = holidays + "#05_05"
+     holidays = holidays + "#07_18"
+     holidays = holidays + "#08_11"
+     holidays = holidays + "#09_19"
+     holidays = holidays + "#09_22"
+     holidays = holidays + "#10_10"
+     holidays = holidays + "#11_03"
+     holidays = holidays + "#11_23"
+     holidays = holidays + "#12_23"
+
+
+
+// year end
+     holidays = holidays + "#12_29"
+     holidays = holidays + "#12_30"
+     holidays = holidays + "#12_31#"
+
+
